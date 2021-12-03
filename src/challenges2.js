50e1fb5eefb667047508541b148f7e1f9d3e5305
Bruno Minelli <minellibruno@hotmail.com>
2021-12-03 15:53:22 -0300

    Conclusão do desafio 11.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 /* Bruno Minelli - T19 - A */
 // Desafio 10
 function techList(array, name) {
@@ -10,7 +11,7 @@ function techList(array, name) {
 
   array.sort();
   for (let index = 0; index < array.length; index += 1) {
-    let object = { tech: array[index], name};
+    let object = { tech: array[index], name };
     newArray.push(object);
   }
 
@@ -18,8 +19,34 @@ function techList(array, name) {
 }
 
 // Desafio 11
-function generatePhoneNumber() {
+function checkPhoneNumber(array, digit) {
+  let repeat = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === digit) {
+      repeat += 1;
+    }
+  }
+  return repeat;
+}
+
+// eslint-disable-next-line complexity
+function generatePhoneNumber(array) {
   // seu código aqui
+  let message = '';
+  let phone = `(${array.slice(0, 2).join('')}) ${array.slice(2, 7).join('')}-${array.slice(7, 11).join('')}`;
+  if (array.length !== 11) {
+    message = 'Array com tamanho incorreto.';
+    return message;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 9 || array[index] < 0 || checkPhoneNumber(array, array[index]) >= 3) {
+      message = 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  if (message === '') {
+    return phone;
+  }
+  return message;
 }
 
 // Desafio 12
